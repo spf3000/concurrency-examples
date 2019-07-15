855843d6acdbd739b3723e7926861f13efe9f0c1
Alan Devlin <a.devlin@elsevier.com>
2019-07-15 10:27:54 +0100

    add Java

--- src/main/scala/example/FutureHello.scala
+++ src/main/scala/example/FutureHello.scala
@@ -7,18 +7,25 @@ import java.util.concurrent.Executors
 object FutureHello {
 
   def hello(n: Int)(implicit ec: ExecutionContext) = Future {
+//    blocking {
     Thread.sleep(2000)
     println(s"hello from thread ${Thread.currentThread.getName()} future $n")
+//    }
   }
 
   def main(args: Array[String]): Unit = {
     implicit val ec: ExecutionContext = ExecutionContext.global
 
+    for {
+      _ <- Future(println("porca"))
+      _ <- Future(println("miseria"))
+    } yield ()
+
     println(s"hello from thread ${Thread.currentThread.getName()} main method")
 
-    val l = (1 to 10).toList
+    val l = (1 to 100).toList
     val list = l.map(n => hello(n)) //(fixedPool))
-    Future.sequence(list)
+    Await.ready(Future.sequence(list), 20 seconds)
 
     //    blocking {
     //}
@@ -33,10 +40,11 @@ object FutureHello {
 //     val fixedPool = ExecutionContext.fromExecutor(fixedExecutor)
 //    val unboundedExecutor  = Executors.newCachedThreadPool()
 //    val blockPool = ExecutionContext.fromExecutor(unboundedExecutor)
-    //   Future.sequence(list)
-    //     .onComplete{
-    //     case either => unboundedExecutor.shutdown(); fixedExecutor.shutdown()
-    //   }
+//
+//   Future.sequence(list)
+//     .onComplete{
+//     case either => unboundedExecutor.shutdown(); fixedExecutor.shutdown()
+//   }
   }
 
 }
---
+++ src/main/scala/example/Java.scala
@@ -0,0 +1,21 @@
+object Java {
+
+  def hello(n: Int) =
+    new Thread {
+      override def run(): Unit = {
+        //      Thread.sleep(1000)
+        println(
+          s"hello from thread ${Thread.currentThread.getName()} java $n"
+        )
+      }
+    }
+
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 100) {
+      val thread1 = hello(i)
+      thread1.start
+    }
+    sys.exit()
+  }
+
+}
